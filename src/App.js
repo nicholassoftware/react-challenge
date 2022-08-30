b18d25f04bd089a31c39798aa35ca332927e5a23
zacfree <zacharyfreedel@gmail.com>
2022-08-30 17:07:51 -0400

    Initialize project using Create React App and update requirement in READEME and add nested list...

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+// Questions:
+// 1. Load data from local file (path: “https://ac.aws.citizennet.com/assets/qspreviews/qs_interview_data.json”)
+// 2. Use the screenshot as an example, implement a generic function for reading any JSON file in that format, then display the top 12 brands based on audience_size. We always want to have 4 items in one row.
+// 3. Add a hover state with a dark, semi-transparent overlay and display the ID of the hovered brand.
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          Let's start here
+        </div>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
