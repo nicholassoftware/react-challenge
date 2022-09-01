cae4f66872ddf17ca75dccfcb28c7b619fe0952a
zacfree <zacharyfreedel@gmail.com>
2022-09-01 17:43:11 +0100

    add: test task

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { defaultData } from "./defaults";
 
 // Questions:
 // 1. Load data from local file (path: “https://ac.aws.citizennet.com/assets/qspreviews/qs_interview_data.json”)
@@ -6,13 +8,53 @@ import './App.css';
 // 3. Add a hover state with a dark, semi-transparent overlay and display the ID of the hovered brand.
 
 function App() {
+  const [data, setData] = useState(defaultData);
+
+  /**
+   * @param {typeof data} arr
+   * @returns {typeof data} sorted array by audience_size desc
+   */
+  const sortData = (arr) => {
+    return arr.sort((a, b) => {
+      if (a.source_items.audience_size < b.source_items.audience_size) return 1;
+      if (a.source_items.audience_size > b.source_items.audience_size)
+        return -1;
+      return 0;
+    });
+  };
+
+  const fetchData = async () => {
+    try {
+      const response = await (
+        await fetch(
+          "https://ac.aws.citizennet.com/assets/qspreviews/qs_interview_data.json"
+        )
+      ).json();
+      setData(sortData(response?.data || []));
+      console.info(response);
+    } catch (error) {
+      console.error(error.message);
+      setData(sortData(defaultData));
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.info(data);
+
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          Let's start here
+          {data.map((item) => (
+            <img
+              key={item.social_media_pages.id}
+              src={item.social_media_pages.picture}
+              alt="pic"
+            />
+          ))}
         </div>
-        
       </header>
     </div>
   );
---
+++ src/defaults.js
@@ -0,0 +1,275 @@
+const defaultData = [
+  {
+    name: "Allure",
+    svg: "allure",
+    source_items: {
+      audience_size: 2754370,
+      id: "6003546862198",
+      name: "Allure (magazine)",
+    },
+    social_media_pages: {
+      category: "Media/News Company",
+      id: "24810773606",
+      name: "Allure",
+      picture:
+        "https://scontent-iad3-1.xx.fbcdn.net/v/t1.18169-1/13096026_10154105228943607_3473782218096997626_n.jpg?stp=cp0_dst-jpg_p50x50&_nc_cat=1&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=AFitorrUeIgAX9K1vap&_nc_ht=scontent-iad3-1.xx&edm=AJdBtusEAAAA&oh=00_AT993Ft0eZn3QrhTlf3FZfMq4hCvCTY9YY_bG5Ace0crAg&oe=63238386",
+      popularity: 1417512,
+    },
+  },
+  {
+    name: "Architectural Digest",
+    svg: "ad",
+    source_items: {
+      audience_size: 7012660,
+      id: "6003159508834",
+      name: "Architectural Digest",
+    },
+    social_media_pages: {
+      category: "Magazine",
+      id: "43794751042",
+      name: "Architectural Digest",
+      picture:
+        "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-1/cp0/p50x50/70657615_10157290676396043_6110168716085821440_n.jpg?_nc_cat=1&ccb=1-3&_nc_sid=dbb9e7&_nc_ohc=fKg4Iad6Zj8AX-Pje0B&_nc_ht=scontent-iad3-1.xx&tp=27&oh=a94f102508060350ed06becef5112e20&oe=606D51F3",
+      popularity: 4297326,
+    },
+  },
+  {
+    name: "Ars Technica",
+    svg: "arstechnica",
+    source_items: {
+      audience_size: 400500,
+      id: "6003183616966",
+      name: "Ars Technica",
+    },
+    social_media_pages: {
+      category: "Media/News Company",
+      id: "19374573752",
+      name: "Ars Technica",
+      picture:
+        "https://scontent-iad3-1.xx.fbcdn.net/v/t1.18169-1/10411094_10152417875818753_7442914011700714865_n.png?stp=cp0_dst-png_p50x50&_nc_cat=102&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=mAB3IcmZzyEAX-CjT4y&_nc_ht=scontent-iad3-1.xx&edm=AJdBtusEAAAA&oh=00_AT9rwOAb_T-qXopnkbi9-Z_wDA8sisHdzmkwtwHheYsf4g&oe=6325CE4E",
+      popularity: 368324,
+    },
+  },
+  {
+    name: "Bon Appetit",
+    svg: "bonappetit",
+    source_items: {
+      audience_size: 9022350,
+      id: "6003288054951",
+      name: "Bon AppÃ©tit",
+    },
+    social_media_pages: {
+      category: "Magazine",
+      id: "37940325366",
+      name: "Bon AppÃ©tit Magazine",
+      picture:
+        "https://scontent-iad3-1.xx.fbcdn.net/v/t1.6435-1/66822479_10162074923580367_7253832045023985664_n.jpg?stp=cp0_dst-jpg_p50x50&_nc_cat=1&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=CpM8ONo_0WQAX_W-zyJ&_nc_ht=scontent-iad3-1.xx&edm=AJdBtusEAAAA&oh=00_AT8JHgQFmkcH5tBA2a9X_F_5wwgZfIzbvg0Fg4YC2IOuYw&oe=632532A8",
+      popularity: 2379968,
+    },
+  },
+  {
+    name: "CondÃ© Nast Traveler",
+    svg: "cntraveler",
+    source_items: {
+      audience_size: 10129439,
+      id: "6003070150915",
+      name: "CondÃ© Nast Traveler",
+    },
+    social_media_pages: {
+      category: "Magazine",
+      id: "21317493981",
+      name: "CondÃ© Nast Traveler",
+      picture:
+        "https://scontent-iad3-1.xx.fbcdn.net/v/t1.6435-1/56664628_10157400900553982_4262255004007530496_n.jpg?stp=cp0_dst-jpg_p50x50&_nc_cat=1&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=0krQU8lnfQAAX9KSW8K&_nc_oc=AQnMVC_n3pplWgXCS7VHRhYfiV3o7SYgp2U8HDw6-afHBTVd7ZQUrao-9up41zHn1Zg&_nc_ht=scontent-iad3-1.xx&edm=AJdBtusEAAAA&oh=00_AT_I7ackEY2Gibwkct5oeceBMf7JiCVeB5n8tPPAucSOYg&oe=6325C76B",
+      popularity: 1802730,
+    },
+  },
+  {
+    name: "Epicurious",
+    svg: "epicurious",
+    source_items: {
+      audience_size: 1947080,
+      id: "6004009784506",
+      name: "Epicurious",
+    },
+    social_media_pages: {
+      category: "Food & Beverage Company",
+      id: "12770210203",
+      name: "Epicurious",
+      picture:
+        "https://scontent-iad3-1.xx.fbcdn.net/v/t1.18169-1/10436192_10155195782900204_1712537691291794024_n.png?stp=cp0_dst-png_p50x50&_nc_cat=104&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=3M2JfNQnRPUAX_VEwsB&_nc_ht=scontent-iad3-1.xx&edm=AJdBtusEAAAA&oh=00_AT_BQQOk7neyrmsx776zPNE0hYmH422oDspn8fL3rEGERA&oe=6322D9C1",
+      popularity: 1198827,
+    },
+  },
+  {
+    name: "Glamour",
+    svg: "glamour",
+    source_items: {
+      audience_size: 64807960,
+      id: "6003326451509",
+      name: "Glamour (magazine)",
+    },
+    social_media_pages: {
+      category: "Magazine",
+      id: "26815555478",
+      name: "Glamour",
+      picture:
+        "https://scontent-iad3-1.xx.fbcdn.net/v/t1.6435-1/31131098_10156151334225479_6172055329350615040_n.png?stp=cp0_dst-png_p50x50&_nc_cat=1&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=CtsIpV3TU7oAX-shujD&_nc_oc=AQmikZACKeNISsoNHbwa4gT73AGUQiSCwXJtByFScplBBH-YNnQZTet6dWZCXkhfI30&_nc_ht=scontent-iad3-1.xx&edm=AJdBtusEAAAA&oh=00_AT83OXPwyrPfL9TPHdt-4TRn1OfCuYH-7DTP9avh9BrUlA&oe=63258DAE",
+      popularity: 5604123,
+    },
+  },
+  {
+    name: "GQ",
+    svg: "gq",
+    source_items: {
+      audience_size: 17374899,
+      id: "6003023143356",
+      name: "GQ",
+    },
+    social_media_pages: {
+      category: "Magazine",
+      id: "7962048097",
+      name: "GQ",
+      picture:
+        "https://scontent-iad3-1.xx.fbcdn.net/v/t1.6435-1/51713736_10158172555063098_2208758842605436928_n.jpg?stp=cp0_dst-jpg_p50x50&_nc_cat=1&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=8b8CAOUXlvcAX_8Uh2-&_nc_ht=scontent-iad3-1.xx&edm=AJdBtusEAAAA&oh=00_AT8neZy_SNMjsK6N0AhsAwV0PJSNj5QOVrY9YvUWo3oU7w&oe=63263208",
+      popularity: 2779290,
+    },
+  },
+  {
+    name: "The New Yorker",
+    svg: "newyorker",
+    source_items: {
+      audience_size: 17623240,
+      id: "6003401313347",
+      name: "The New Yorker",
+    },
+    social_media_pages: {
+      category: "Magazine",
+      id: "9258148868",
+      name: "The New Yorker",
+      picture:
+        "https://scontent-iad3-1.xx.fbcdn.net/v/t1.6435-1/84687789_10157179446178869_8112177586932547584_n.png?stp=cp0_dst-png_p50x50&_nc_cat=1&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=uoEPVRBVEkwAX8yYo3P&_nc_ht=scontent-iad3-1.xx&edm=AJdBtusEAAAA&oh=00_AT9jsdf-ZqcR4ulVGIna7oPOWnLQOlLikg2AZvpadUVn7w&oe=63241E8D",
+      popularity: 4374251,
+    },
+  },
+  {
+    name: "Pitchfork",
+    svg: "pitchfork",
+    source_items: {
+      audience_size: 659160,
+      id: "6003329810777",
+      name: "Pitchfork",
+    },
+    social_media_pages: {
+      category: "Media/News Company",
+      id: "28015075999",
+      name: "Pitchfork",
+      picture:
+        "https://scontent-iad3-1.xx.fbcdn.net/v/t1.18169-1/17191115_10154979282611000_807401602764105743_n.jpg?stp=cp0_dst-jpg_p50x50&_nc_cat=1&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=1RZq1qw7OicAX9MHqZl&_nc_ht=scontent-iad3-1.xx&edm=AJdBtusEAAAA&oh=00_AT-u30ZehWp0NrBHQAVeCAg02qwu-xQMHv73jTA8f9CbqA&oe=632340EE",
+      popularity: 1411207,
+    },
+  },
+  {
+    name: "Self",
+    svg: "self",
+    source_items: {
+      audience_size: 136242830,
+      id: "6003512851365",
+      name: "Self (magazine)",
+    },
+    social_media_pages: {
+      category: "Magazine",
+      id: "8908243810",
+      name: "SELF",
+      picture:
+        "https://scontent-iad3-1.xx.fbcdn.net/v/t1.6435-1/36250829_10155673884808811_4198757438704320512_n.jpg?stp=cp0_dst-jpg_p50x50&_nc_cat=1&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=2-FS7K3at_8AX8SskCX&_nc_ht=scontent-iad3-1.xx&edm=AJdBtusEAAAA&oh=00_AT-JBRkIabLjsHEhARGQCzII9M_kL0nSvSdXNROgswjFXw&oe=63263F9A",
+      popularity: 1710229,
+    },
+  },
+  {
+    name: "Teen Vogue",
+    svg: "teenvogue",
+    source_items: {
+      audience_size: 4098860,
+      id: "6003094903858",
+      name: "Teen Vogue",
+    },
+    social_media_pages: {
+      category: "News & Media Website",
+      id: "6636341311",
+      name: "Teen Vogue",
+      picture: "",
+      popularity: 5709701,
+    },
+  },
+  {
+    name: "Vanity Fair",
+    svg: "Vanity_Fair_Logo",
+    source_items: {
+      audience_size: 16813627,
+      id: "6003070845715",
+      name: "Vanity Fair (magazine)",
+    },
+    social_media_pages: {
+      category: "Magazine",
+      id: "8810407571",
+      name: "Vanity Fair",
+      picture:
+        "https://scontent-iad3-1.xx.fbcdn.net/v/t1.6435-1/39169724_10156100789677572_7064132170012426240_n.png?stp=cp0_dst-png_p50x50&_nc_cat=1&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=pQQGw2KuZD8AX91OLF0&_nc_ht=scontent-iad3-1.xx&edm=AJdBtusEAAAA&oh=00_AT_o2E4JoEOnllOzNVxDT1DyQhQBgNMgmhYRndh46wsuRQ&oe=6326434B",
+      popularity: 2973074,
+    },
+  },
+  {
+    name: "Vogue",
+    svg: "VOGUE_LOGO",
+    source_items: {
+      audience_size: 77145652,
+      id: "6003552041427",
+      name: "Vogue (magazine)",
+    },
+    social_media_pages: {
+      category: "Magazine",
+      id: "42933792278",
+      name: "Vogue",
+      picture:
+        "https://scontent-iad3-1.xx.fbcdn.net/v/t39.30808-1/293333191_10159760309827279_1900656779478001128_n.jpg?stp=cp0_dst-jpg_p50x50&_nc_cat=1&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=FIwyyCNrx1gAX-Zl66B&_nc_ht=scontent-iad3-1.xx&edm=AJdBtusEAAAA&oh=00_AT-GPAYoCejF7hVvcgjez3eSigq_JIyCdmBjU3b0Si9FEA&oe=63045722",
+      popularity: 9015092,
+    },
+  },
+  {
+    name: "W",
+    svg: "w-magazine-vector-logo",
+    source_items: {
+      audience_size: 4931550,
+      id: "6003196100855",
+      name: "W (magazine)",
+    },
+    social_media_pages: {
+      category: "Magazine",
+      id: "40621875003",
+      name: "W Magazine",
+      picture:
+        "https://scontent-iad3-1.xx.fbcdn.net/v/t1.6435-1/71023988_10162215008630004_5918106793745055744_n.jpg?stp=cp0_dst-jpg_p50x50&_nc_cat=1&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=6NRFEhr6TdIAX-ylSW0&_nc_ht=scontent-iad3-1.xx&edm=AJdBtusEAAAA&oh=00_AT--JM4QldpCwW98j_L-VNtVoJEX4sPtXnQ7a8N23NWb-A&oe=63262DBE",
+      popularity: 1760689,
+    },
+  },
+  {
+    name: "Wired",
+    svg: "Wired_logo",
+    source_items: {
+      audience_size: 12216460,
+      id: "6003136001590",
+      name: "Wired (magazine)",
+    },
+    social_media_pages: {
+      category: "Media/News Company",
+      id: "19440638720",
+      name: "WIRED",
+      picture:
+        "https://scontent-iad3-1.xx.fbcdn.net/v/t1.6435-1/71023988_10162215008630004_5918106793745055744_n.jpg?stp=cp0_dst-jpg_p50x50&_nc_cat=1&ccb=1-7&_nc_sid=dbb9e7&_nc_ohc=6NRFEhr6TdIAX-ylSW0&_nc_ht=scontent-iad3-1.xx&edm=AJdBtusEAAAA&oh=00_AT--JM4QldpCwW98j_L-VNtVoJEX4sPtXnQ7a8N23NWb-A&oe=63262DBE",
+      popularity: 2985481,
+    },
+  },
+];
+
+export { defaultData };
